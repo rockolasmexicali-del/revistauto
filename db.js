bf417ef1ecd9f1757bb7ab8bde5ff8b39b826dee
rockolasmexicali-del <rockolasmexicali@gmail.com>
2026-08-08 21:21:06 -0700

    se reparaon emojis

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "1.4.5"; // Incrementa este valor cada vez que actualices el catálogo o estructura
+const APP_VERSION = "1.4.6"; // Incrementa este valor cada vez que actualices el catálogo o estructura
 
 const defaultCatalogData = {
     makes: [
