dda300ff148500e75eabf897f43c347023735d19
rockolasmexicali-del <rockolasmexicali@gmail.com>
2026-08-16 00:20:04 -0700

    varios
    se subio la capzula de deslice

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "2.1.3"; // Incrementa este valor cada vez que actualices el catálogo o estructura
+const APP_VERSION = "2.1.4"; // Incrementa este valor cada vez que actualices el catálogo o estructura
 
 const defaultCatalogData = {
     makes: [
